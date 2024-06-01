6b71ab40b78fde1405c939fda4b20ba176b7ec95
althof-z <althof.zpv@gmail.com>
2024-06-01 19:48:04 +0700

    add Locale and Theme Context

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import LocaleContext from "../contexts/LocaleContext";
+import ThemeContext from "../contexts/ThemeContext";
 import NoteAppBody from "./NoteAppBody";
 import NoteAppHeader from "./NoteAppHeader";
 
 function NoteApp() {
+  const [locale, setLocale] = useState(localStorage.getItem("locale") || "id");
+  const [Theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const toggleLocale = () => {
+    const newLocale = locale === "id" ? "en" : "id";
+    localStorage.setItem("locale", newLocale);
+    setLocale(newLocale);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", Theme);
+  }, [Theme]);
+
+  const toggleTheme = () => {
+    const newTheme = Theme === "light" ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  };
+
   return (
     <div className="app-container">
-      <NoteAppHeader />
-      <NoteAppBody />
+      <ThemeContext.Provider value={{ Theme, toggleTheme }}>
+        <LocaleContext.Provider value={{ locale, toggleLocale }}>
+          <NoteAppHeader />
+          <NoteAppBody />
+        </LocaleContext.Provider>
+      </ThemeContext.Provider>
     </div>
   );
 }
--- src/components/NoteAppHeader.jsx
+++ src/components/NoteAppHeader.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FiLogOut } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { HiOutlineLanguage } from "react-icons/hi2";
+import { FaRegSun, FaMoon } from "react-icons/fa6";
+import LocaleContext from "../contexts/LocaleContext";
+import ThemeContext from "../contexts/ThemeContext";
+import PropTypes from "prop-types";
 
-const NoteAppHeader = () => {
+function NoteAppHeader() {
+  const { locale, toggleLocale } = useContext(LocaleContext);
+  const { Theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <header>
       <h1>
-        <Link to="/">Personal Notes</Link>
+        <Link to="/">{locale === "id" ? "Catatan Pribadi" : "Personal Note"}</Link>
       </h1>
+      <nav className="navigation">
+        <ul>
+          <li>
+            <button onClick={toggleTheme} className="button-logout">
+              {Theme === "light" ? <FaRegSun /> : <FaMoon />}
+            </button>
+          </li>
+          <li>
+            <button onClick={toggleLocale} className="button-logout">
+              <HiOutlineLanguage />
+            </button>
+          </li>
+          <li>
+            {/* <Link to="/">
+              <button onClick={logout} className="button-logout">
+                {name} <FiLogOut />
+              </button>
+            </Link> */}
+          </li>
+        </ul>
+      </nav>
     </header>
   );
-};
+}
+
+// NoteAppHeader.propTypes = {
+//   logout: PropTypes.func.isRequired,
+// };
 
 export default NoteAppHeader;
